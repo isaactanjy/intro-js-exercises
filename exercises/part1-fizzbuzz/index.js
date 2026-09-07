8eeebfc22e6fe20e187f800bb037f2ee3c4f1afa
yjti <isaactan@upenn.edu>
2026-09-07 14:03:08 -0400

    Update

--- exercises/part1-fizzbuzz/index.js
+++ exercises/part1-fizzbuzz/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /* =====================
 
 # Exercise: FizzBuzz
@@ -25,8 +26,39 @@ Start code
 
 ===================== */
 
+// Step 1
 
+for (let i = 1; i <= 100; i++) {
+    console.log(i)
+}
 
+// Step 2
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+    console.log(i);
+    }
+}
+
+// Step 3
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+    console.log(i);
+    }
+}
+
+// It is important that the FizzBuzz 'if' line is on top. Why? Because both the fizzes and the buzzes get assigned, so the line of code that is last does not actually get evaluated. This is why it has to go first.
 
 /* =====================
 
--- exercises/part2-basic-functions/index.js
+++ exercises/part2-basic-functions/index.js
@@ -13,7 +13,10 @@ in years. (Let's just assume this person was born January 1 at 12:00 AM)
 Example: "age(2000) should return 26"
 ===================== */
 
-let age = (birthYear) => {};
+
+let age = (birthYear) => {
+    return (2026 - birthYear);
+};
 
 console.log('age success:', age(1971) === 55);
 
@@ -22,7 +25,9 @@ Instructions: Write a function that adds one to the number provided
 Example: "plusOne(2) should return 3"
 ===================== */
 
-let plusOne = () => {};
+let plusOne = (num) => {
+    return (++num)
+};
 
 console.log('plusOne success:', plusOne(99) === 100);
 
@@ -31,7 +36,9 @@ Instructions: Write a function that multiplies the number provided by three
 Example: "timesThree(2) should return 6"
 ===================== */
 
-let timesThree = () => {};
+let timesThree = (num) => {
+    return (num * 3)
+};
 
 console.log('timesThree success:', timesThree(33) === 99);
 
@@ -40,7 +47,9 @@ Instructions: Write a function that adds two given numbers
 Example: "add(2, 3) should return 5"
 ===================== */
 
-let add = () => {};
+let add = (a, b) => {
+    return (a + b)
+};
 
 console.log('add success:', add(4, 6) === 10);
 
@@ -49,7 +58,9 @@ Instructions: Write a function that multiplies two given numbers
 Example: "multiply(2, 3) should return 6"
 ===================== */
 
-let multiply = () => {};
+let multiply = (a, b) => {
+    return (a * b)
+};
 
 console.log('multiply success:', multiply(4, 6) === 24);
 
@@ -57,7 +68,9 @@ console.log('multiply success:', multiply(4, 6) === 24);
 Instructions: Write a function that returns true if a number is even.
 ===================== */
 
-let isEven = () => {};
+let isEven = (num) => {
+    return (num % 2 === 0)
+};
 
 console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 
@@ -65,7 +78,9 @@ console.log('isEven success:', isEven(2) === true && isEven(3) === false);
 Instructions: Write a function that returns true if a number is odd.
 ===================== */
 
-let isOdd = () => {};
+let isOdd = (num) => {
+    return (num % 2 !== 0)
+};
 
 console.log('isOdd success:', isOdd(4) === false && isOdd(5) === true);
 
@@ -74,7 +89,9 @@ Instructions: "Write a function that returns the value of an array at a specifie
 Example: "valueAtIndex(['Mercury', 'Venus', 'Earth', 'Mars'], 2) should return 'Earth'"
 ===================== */
 
-let valueAtIndex = () => {};
+let valueAtIndex = (arr, index) => {
+    return arr[index];
+};
 
 console.log('valueAtIndex success:', valueAtIndex([9, 8, 7, 6, 5], 2) === 7);
 
@@ -83,7 +100,9 @@ Instructions: "Write a function that returns the value of an object at a specifi
 Example: "valueAtKey({name: 'Nathan', age: 27}, 'name') should return 'Nathan'"
 ===================== */
 
-let valueAtKey = () => {};
+let valueAtKey = (obj, key) => {
+    return obj[key];
+};
 
 console.log('valueAtKey success:', valueAtKey({ 'foo': 'bar' }, 'foo') === 'bar');
 
@@ -92,7 +111,9 @@ Instructions: "Write a function that returns the index of a value in an array"
 Example: "indexForValue(['Mercury', 'Venus', 'Earth', 'Mars'], 'Earth') should return 2"
 ===================== */
 
-let indexForValue = () => {};
+let indexForValue = (arr, value) => {
+    return arr.indexOf(value);
+};
 
 console.log('indexForValue success:', indexForValue([9, 8, 7, 6, 5], 7) === 2);
 
@@ -101,6 +122,8 @@ Instructions: "Write a function that returns the key of a value in an object"
 Example: "keyForValue({name: 'Nathan', age: 27}, 'Nathan') should return 'name'"
 ===================== */
 
-let keyForValue = () => {};
+let keyForValue = (obj, value) => {
+    return Object.keys(obj).find(key => obj[key] === value); // check with Mjumbe on why this undefined(?) function works?
+};
 
 console.log('keyForValue success:', keyForValue({ 'foo': 'bar', 'baz': 'qux' }, 'bar') === 'foo');
